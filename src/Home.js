2f3b23dd8110f4015114f2b8ef5890489630518e
jimmymachine <jimmyhranowskyj@gmail.com>
2024-09-24 10:47:59 -0400

    Made styleguide and Home CSS

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import './Home.css';
+import React from 'react';  
+
+
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="div">
+        <div className="frame">
+          <header className="text-wrapper-2">Movie</header>
+          <div className="text-wrapper">Login/Sign up</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
