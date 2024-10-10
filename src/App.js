2b0c940c3f53b3a24957a3497ec5ef5a69e18efb
jimmymachine <jimmyhranowskyj@gmail.com>
2024-10-10 15:52:08 -0400

    Added Search Bar to Homepage
    - not currently functional
    - issue in accessing title of movie objects using Array.filter() in App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,66 @@
 import './App.css';
-import React from 'react'; 
-import MovieCard from './components/MovieCard'; 
+import React from 'react';
+import MovieCard from './components/MovieCard';
 import { useEffect } from 'react';
 import { useState } from 'react';
-
+import SearchIcon from './components/search.svg';
 
 const App = () => {
-  const [movieData, setMovieData] = useState([]);
+  const [movieData, setMovieData] = useState();
+  const [searchTerm, setSearchTerm] = useState('');
+  const DATABASE_URL = 'http://localhost:3001/movies';
+
   useEffect(() => {
-      fetch('http://localhost:3001/movies')
+    fetch(DATABASE_URL)
       .then((res) => res.json())
       .then(data => setMovieData(data.rows))
-      .catch(err => console.log(err));        
+      .catch(err => console.log(err));
   }, []);
+  console.log(movieData)
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    setSearchTerm(value);
+    filterMovies(value);
+    console.log(searchTerm)
+    console.log(filterMovies(value))
+  }
+
+  const filterMovies = (searchTerm) => {
+    const filteredMovies = movieData.filter((movie) =>
+      movie.title.includes(searchTerm)
+    );
+    setMovieData(filteredMovies)
+  }
 
   if (!movieData || movieData.length === 0) {
-      return <div>Loading...</div>;
-    } 
-    console.log(movieData);
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="app">
       <h1>Hawk Tuah Movies</h1>
 
+      <div className="search">
+        <input
+          placeholder="Search for a movie"
+          value = {searchTerm}
+          onChange={(e) =>             
+            setSearchTerm(e.target.value)
+          }
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick = {handleInputChange}
+        />
+      </div>
 
-     <div className="container">
+      
+      <div className="container">
         {movieData.map((movie) => (
-          <MovieCard movie = {movie} />
+          <MovieCard 
+          movie={movie} />
         ))}
       </div>
     </div>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,25 +2,28 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 
-const MovieCard = (movie) => {
-return (
+const MovieCard = (movie, searchTerm) => {
+  console.log(movie)
+  return (
     <div className="movie">
-        <div>
-            <p>{movie.movie.title}</p>
-        </div>
+      <div>
+        <p>{movie.movie.title}</p>
+      </div>
 
-        <div>
-            <img src={movie.movie.posterurl} alt="Movie Poster" />
-        </div>
+      <div>
+        <img src={movie.movie.posterurl} alt="Movie Poster" />
+        onMouseEnter
+      </div>
 
-        <div>
-            <span>{movie.movie.status}</span>
-            <h3>{movie.title}</h3>
-        </div>
+      <div>
+        <span>{movie.movie.status}</span>
+        {/* I feel like it looks better without this
+        <h3>{movie.movie.title}</h3> */}
+      </div>
     </div>
 
-    
-);
+  );
 }
+
 
 export default MovieCard;
