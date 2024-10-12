3dfa99b2238275acff0b699fea9a783d9e8eb1b1
jimmymachine <jimmyhranowskyj@gmail.com>
2024-10-11 20:22:10 -0400

    Added buttons n forms n stuff for Admin View

--- src/AdminView.js
+++ src/AdminView.js
@@ -3,12 +3,54 @@ import './App.css';
 import MovieCard from './components/MovieCard';
 
 const App = () => {
-  return(
-  <div className="app">
-    <div style={{ boxShadow: "0px 13px 10px -7px rgba(0, 0, 0, 0.1)" }} className="add-movie">
-      <button className="" >Add Movie</button>
+  const [showAdd, setShowAdd] = useState(false);
+  const [showDelete, setShowDelete] = useState(false);
+
+  /* populate the movie form with the input and send to database?*/
+  const [movieForm, setMovieForm] = useState();
+
+  function showAddComponent() {
+    setShowAdd(!showAdd);
+  }
+
+  function showDeleteComponent() {
+    setShowDelete(!showDelete)
+  }
+
+
+  return (
+    <div className="app">
+      <div className="top-bar">
+        <button className="login-button">Logout</button>
+        <button className="preview-button">Preview User View</button>
+      </div>
+      <div className="button-container">
+        <button className="add-movie-button" onClick={showAddComponent}>Add Movie</button>
+      </div>
+      {showAdd && (
+        <div className="form-container">
+          <input type="text" placeholder="Movie Title" />
+          <input type="text" placeholder="Description" />
+          <input type="text" placeholder="Poster" />
+          <input type="text" placeholder="Trailer" />
+          <input type="text" placeholder="Status" />
+          <input type="text" placeholder="ID" />
+          <button className="add-movie-button">Save Movie</button>
+
+        </div>
+      )}
+      {/* should reveal a component for finding a movie to */}
+      <button className="delete-movie-button" onClick={showDeleteComponent}>Delete Movie</button>
+
+      {showDelete && (
+        <div className="form-container">
+          <input className="text-input"type="text" placeholder="Search for movie to delete"/>
+
+
+        </div>
+      )}
+
     </div>
-  </div>
 
   );
 };
