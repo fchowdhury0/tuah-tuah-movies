f061ba9c055fb9aa8bc5ff13d8109810007bffde
jimmymachine <jimmyhranowskyj@gmail.com>
2024-10-12 14:02:11 -0400

    Added Formik for form handling
    - This is for the add movie component
    - Should format input nicely for backend use

--- src/components/AdminView.jsx
+++ src/components/AdminView.jsx
@@ -1,12 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import './AdminView.css'
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const App = () => {
   const [showAdd, setShowAdd] = useState(false);
   const [showDelete, setShowDelete] = useState(false);
 
   /* populate the movie form with the input and send to database?*/
-  const [movieForm, setMovieForm] = useState();
+  const formik = useFormik({
+    initialValues: {
+      movieTitle: '',
+      description: '',
+      posterUrl: '',
+      trailerUrl: '',
+      status: '',
+      id: ''
+    }
+  })
+
+  console.log('Form values', formik.values)
 
   function showAddComponent() {
     setShowAdd(!showAdd);
@@ -18,7 +31,7 @@ const App = () => {
 
 
   return (
-    <div className="app">
+    <div>
       <div className="top-bar">
         <button className="login-button">Logout</button>
         <button className="preview-button">Preview User View</button>
@@ -28,13 +41,45 @@ const App = () => {
       </div>
       {showAdd && (
         <div className="form-container">
-          <input type="text" placeholder="Movie Title" />
-          <input type="text" placeholder="Description" />
-          <input type="text" placeholder="Poster" />
-          <input type="text" placeholder="Trailer" />
-          <input type="text" placeholder="Status" />
-          <input type="text" placeholder="ID" />
-          <button className="add-movie-button">Save Movie</button>
+
+            <input
+              type="text"
+              id="movieTitle"
+              placeholder="Movie Title"
+              onChange={formik.handleChange}
+              value={formik.values.movieTitle} />
+            <input 
+              type="text"
+              id="description"
+              placeholder="Description"
+              onChange={formik.handleChange}
+              value={formik.values.description} />
+            <input
+              type="text"
+              id="poster"
+              placeholder="PosterUrl"
+              onChange={formik.handleChange}
+              value={formik.values.posterUrl} />
+            <input
+              type="text"
+              id="poster"
+              placeholder="TrailerUrl"
+              onChange={formik.handleChange}
+              value={formik.values.trailerUrl}/>
+            <input
+              type="text"
+              id="status"
+              placeholder="Status" 
+              onChange={formik.handleChange}
+              value={formik.values.status}/>
+            <input
+              type="text"
+              id="id"
+              placeholder="ID"             
+              onChange={formik.handleChange}
+              value={formik.values.id}/>
+            <button className="add-movie-button">Submit</button>
+
 
         </div>
       )}
@@ -43,7 +88,7 @@ const App = () => {
 
       {showDelete && (
         <div className="form-container">
-          <input className="text-input"type="text" placeholder="Search for movie to delete"/>
+          <input className="text-input" type="text" placeholder="Search for movie to delete" />
 
 
         </div>
