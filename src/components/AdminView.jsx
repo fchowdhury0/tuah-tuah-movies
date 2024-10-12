b7ff3497936f3f33eacfba40f23cf7c214fd2534
jimmymachine <jimmyhranowskyj@gmail.com>
2024-10-11 21:04:54 -0400

    Merged main
    - AdminView.css got fucked up with merge
    - Will fix later
    - You're so organized John great work

--- src/components/AdminView.jsx
+++ src/components/AdminView.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
-import MovieCard from './components/MovieCard';
+import './AdminView.css'
 
 const App = () => {
   const [showAdd, setShowAdd] = useState(false);
