b18ca1578a0c79b0a8a0a95b70a3c1f61e57aa71
Daisy Gryboski <132220763+daisy-gryboski@users.noreply.github.com>
2024-10-14 15:33:02 -0400

    Order Confirmation with back to home button

--- src/pages/OrderConfirmation/OrderConfirmation.jsx
+++ src/pages/OrderConfirmation/OrderConfirmation.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const OrderConfirmation = () => {
     const location = useLocation();
     const { tickets, total, userInfo, paymentInfo } = location.state || {};
+    const navigate = useNavigate();
 
+    const toHome = () => {
+	navigate('/home');
+    }
     return (
         <div className="order-confirmation" style={{ color: 'white' }}>
             <h2>Order Confirmation</h2>
@@ -18,10 +22,11 @@ const OrderConfirmation = () => {
             <h3>User Information</h3>
             <p>Name: {userInfo.name}</p>
             <p>Email: {userInfo.email}</p>
-            <p>Address: {userInfo.address}</p>
+            <p>Billing Address: {userInfo.address}</p>
             <h3>Payment Information</h3>
             <p>Card Number: {paymentInfo.cardNumber.replace(/\d(?=\d{4})/g, "*")}</p> {/* Mask all but the last 4 digits */}
             <p>Expiration Date: {paymentInfo.expirationDate}</p>
+	    <button onClick={toHome}  className="home-button" type="submit">Done</button> 
         </div>
     );
 };
