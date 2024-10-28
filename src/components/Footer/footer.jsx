5c5867aa2c5f7ed1a15cf44530873f68a3d1c474
jimmymachine <jimmyhranowskyj@gmail.com>
2024-10-27 20:11:48 -0400

    Fixed Admin View issue
    - trying to make NavBar stay within the side borders, but not able to figure it out

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -3,8 +3,10 @@ import './Footer.scss';
 
 
 const Footer = () => {
-  return (
-    <div className="footer">Hawk Tuah Incorporated</div>
+/*  return (
+   // <div className="footer">Hawk Tuah Incorporated</div>
   )
+  */
 }
+
 export default Footer;
