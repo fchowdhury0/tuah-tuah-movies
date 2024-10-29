e8e9c0ce268e01a6a356498679da8589541fae2d
jimmymachine <jimmyhranowskyj@gmail.com>
2024-10-29 14:12:19 -0400

    Restored backend changes

--- cinema-booking/src/main/java/com/example/demo/UserController.java
+++ cinema-booking/src/main/java/com/example/demo/UserController.java
@@ -2,13 +2,33 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/api/user")
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
   
   @Autowired
   private UserRepository userRepository;
 
+  @GetMapping
+  Optional<User> getUser(@RequestParam String username){
+    
+    return userRepository.findByUsername(username);
+  } 
+
+  @PostMapping 
+  User newUser(@RequestBody User newUser) {
+    return userRepository.save(newUser);
+  }
+  
 }
