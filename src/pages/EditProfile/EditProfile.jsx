e316db1a513ef81f641ac707486b42d96940cac1
Daisy Gryboski <132220763+daisy-gryboski@users.noreply.github.com>
2024-10-29 12:13:44 -0400

    navbar update

--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -5,7 +5,7 @@ import './EditProfile.scss';
 import { useParams } from 'react-router-dom'
 import axios from 'axios';
 import api from '../../utils/api.js';
-import { jwtDecode } from 'jwt-decode';
+//import { jwtDecode } from 'jwt-decode';
 
 
 /*check console log for form values*/
@@ -35,7 +35,7 @@ const EditProfile = () => {
         if (!token) {
           throw new Error('No JWT found in sessionStorage');
         }
-        setDecodedToken(jwtDecode(token));
+//        setDecodedToken(jwtDecode(token));
         setUsername(decodedToken.sub)
         console.log(decodedToken.sub)
       } catch (err) {
